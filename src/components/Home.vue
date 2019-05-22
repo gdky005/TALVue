f79fbdc4591af95e59f2d51faf8a9310c28c3c6a
WangQing <wangqing11@100tal.com>
2019-05-22 17:04:37 +0800

    1. 添加自动发布 8.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div style="margin-top: 60px;text-align: center;">
 
-    自动发布7
+    自动发布8
 
     <hr style="height:1px;border:none;border-top:1px dashed #0066CC; margin-left: 200px; margin-right: 200px"/>
 
